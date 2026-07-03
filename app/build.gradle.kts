c5a525243c19399f365ecd6a74aaf0b512118e64
mcuteangel <mpcuteangel@Gmail.com>
2026-07-03 20:41:31 +0330

    fix: update SDK version to 37 and align lifecycle dependencies to 2.11.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 android {
   namespace = "io.github.mojri.hesabyar"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
+  compileSdk = 37
 
   defaultConfig {
     applicationId = "io.github.mojri.hesabyar"
