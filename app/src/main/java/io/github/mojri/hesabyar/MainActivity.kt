5effc8c2de6ca3e63f0119d4d3fea8645ad08af4
mcuteangel <mpcuteangel@Gmail.com>
2026-07-03 12:04:50 +0330

    fix: change authManager to public for accessibility in MainActivity

--- app/src/main/java/io/github/mojri/hesabyar/MainActivity.kt
+++ app/src/main/java/io/github/mojri/hesabyar/MainActivity.kt
@@ -42,7 +42,7 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : FragmentActivity() {
     @Inject
-    private lateinit var authManager: AuthManager
+    lateinit var authManager: AuthManager
 
     private val settingsViewModel: SettingsViewModel by viewModels()
     private val dashboardViewModel: DashboardViewModel by viewModels()
